c20d1ebc660c89d38ca5a59717b1a58172e6cc8f
Saif Al-adilee <saifa9697@gmail.com>
2021-06-02 14:20:07 +0200

    update content

--- js/index.js
+++ js/index.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Consultant"],
+        strings: ["Software Developer", "Software Consultant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Consultant"],
+        strings: ["Software Developer", "Software Consultant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
